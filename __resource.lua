66a89f4948a0e248ba8e76a1ca9e6b1081c7c662
Berkay Ediz İnce <37938768+berkayediz@users.noreply.github.com>
2023-07-12 15:15:58 -0500

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,2 +1,6 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Erenoske, Berkayediz'
+description 'EchoRP MDT Rewrite for QBCore'
 client_script 'client.lua'
